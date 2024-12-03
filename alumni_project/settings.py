92458b2e68c942c43a14626d9ea63980bd088d7f
Jithendra K R <98675683+Jithu666@users.noreply.github.com>
2024-12-03 11:21:46 +0530

    Update settings.py

--- alumni_project/settings.py
+++ alumni_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=et3cvhd3ybzzch86uf*c181db*7-*wts$6ez07iz$)6978cg!"
+SECRET_KEY = "django-insecure-=et3cvhd3ybzzch86uf*c181db*7-*wts$6ez07iz$)!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
